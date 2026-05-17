0b610b9eab4b91ca0690d7c0b7053c7d5383ac81
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-17 17:53:15 -0300

    feat: add PlatformDetector to detect different OSs.

---
+++ lib/platform/platform_detector.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+enum OperatingSystem {
+    linux,
+    windows,
+    macOS,
+}
+
+class PlatformDetector {
+    OperatingSystem detectOperatingSystem() {
+        if (Platform.isLinux) {
+            return OperatingSystem.linux;
+        } else if (Platform.isWindows) {
+            return OperatingSystem.windows;
+        } else if (Platform.isMacOS) {
+            return OperatingSystem.macOS;
+        } else {
+            throw UnsupportedError('yotei error: Platform not supported.');
+        }
+    }
+}
