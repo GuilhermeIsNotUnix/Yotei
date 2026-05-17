38d857edf816007e57bb9b9411a7b5ed3530e3e9
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-17 16:04:23 -0300

    feat: add command router for CLI interface.

--- lib/cli/cli_interface.dart
+++ lib/cli/cli_interface.dart
@@ -4,19 +4,38 @@ class CliInterface {
     CliInterface(this.arguments);
 
     void run() {
-        if(arguments.isEmpty || arguments.first == "help") {
+        if(arguments.isEmpty) {
+            print('yotei error: no argument passed.');
             printUsage();
         } else {
-            detectOS();
+            final command = arguments.first;
+
+            switch(command) {
+                case 'help':
+                    printUsage();
+                    break;
+                case 'status':
+                    break;
+                case 'shutdown':
+                    break;
+                case 'cancel':
+                    break;
+                default:
+                    print('yotei error: No valid command was provided as an argument.');
+                    break;
+            }
         }
     }
 
     void printUsage() {
-        print('yotei error: no argument passed.');
         print('yotei shutdown HH:MM');
         print('\tSchedule the system to shut down at the time HH:MM specified as an argument.');
+        print('yotei status');
+        print('\tShows whether any appointments have been scheduled.');
+        print('yotei cancel');
+        print('\tCancel any previously scheduled shutdowns.');
         print('yotei help');
-        print('\tShows Yotei\'s usage and features');
+        print('\tShows Yotei\'s usage and features.');
     }
 
     void detectOS() {
