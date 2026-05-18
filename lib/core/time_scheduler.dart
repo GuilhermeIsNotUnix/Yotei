9045198763611c2b82b9ea45a150127d9c2ae6df
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-18 15:51:27 -0300

    refactor/fix: TimeScheduler class refactored and fixed bugs.

--- lib/core/time_scheduler.dart
+++ lib/core/time_scheduler.dart
@@ -1,30 +1,31 @@
 class TimeScheduler {
-    final now = DateTime.now();
-
     DateTime constructDateTime(List<int> givenTime) {
-        if(givenTime[0] < now.hour) {
-            final targetTime = DateTime(
+        final now = DateTime.now();
+
+        var targetTime = DateTime(
+            now.year,
+            now.month,
+            now.day,
+            givenTime[0],
+            givenTime[1],
+        );
+
+        if(targetTime.isBefore(now)) {
+            targetTime = DateTime(
                 now.year,
                 now.month,
                 now.day + 1,
                 givenTime[0],
                 givenTime[1],
-            )
-        } else {
-            final targetTime = DateTime(
-                now.year,
-                now.month,
-                now.day,
-                givenTime[0],
-                givenTime[1],
-            )
+            );
         }
 
         return targetTime;
     }
 
-    Duration differenceInSeconds(DateTime targetTime) {
-        final timeRemaining = targetTime.difference(now).inSeconds;
-        return Duration(seconds: timeRemaining);
+    Duration calculateRemainingTime(DateTime targetTime) {
+        final now = DateTime.now();
+
+        return targetTime.difference(now);
     }
 }
