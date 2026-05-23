e274fede731dbdf5844671b199a174778b2fa565
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-23 01:02:50 -0300

    feat: add methods to schedule the shutdown to a target time and also shutdown now

--- lib/platform/linux_shutdown_service.dart
+++ lib/platform/linux_shutdown_service.dart
@@ -1,12 +1,24 @@
 class LinuxShutdownService {
     Future<void> scheduleShutdown(DateTime targetTime) async {
-        final process = await Process.run("shutdown", ["-h", "${targetTime.hour}:${targetTime.minute}"]);
+        if(targetTime.minute < 10) {
+            final minutes = targetTime.minute.toString().padLeft(2, '0');
+            final process = await Process.run("shutdown", ["-h", "${targetTime.hour}:$minutes"]);
 
-        print("Exit code: ${process.exitCode}");
-        print("Output:\n${process.stdout}");
+            print("Exit code: ${process.exitCode}");
+            print("Output:\n${process.stdout}");
 
-        if(process.stderr.isNotEmpty) {
-            print("Errors:${process.stderr}\n");
+            if(process.stderr.isNotEmpty) {
+                print("Errors:${process.stderr}\n");
+            }
+        } else {
+            final process = await Process.run("shutdown", ["-h", "${targetTime.hour}:${targetTime.minute}"]);
+
+            print("Exit code: ${process.exitCode}");
+            print("Output:\n${process.stdout}");
+
+            if(process.stderr.isNotEmpty) {
+                print("Errors:${process.stderr}\n");
+            }
         }
     }
 
