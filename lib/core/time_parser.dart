a10450b11e78412b7d932af9ab66fd47837781ad
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-23 20:57:48 -0300

    fix: Exceptions are cleaner without yotei error quote, because CliInterface will already have that message...

--- lib/core/time_parser.dart
+++ lib/core/time_parser.dart
@@ -1,24 +1,24 @@
 class TimeParser {
     static List<int> parseTime(String givenTime) {
         if(givenTime.isEmpty)
-            throw ArgumentError('yotei error: Given time is empty');
+            throw ArgumentError('Given time is empty');
         
         if(!givenTime.contains(':'))
-            throw ArgumentError('yotei error: Time format is invalid.');
+            throw ArgumentError('Time format is invalid.');
         final timeParts = givenTime.split(':');
 
         if(timeParts.length != 2)
-            throw ArgumentError('yotei error: Time format is invalid.');
+            throw ArgumentError('Time format is invalid.');
         final hours = int.tryParse(timeParts[0]);
         final minutes = int.tryParse(timeParts[1]);
 
         if(hours == null || minutes == null)
-            throw ArgumentError('yotei error: Time contains non-numeric values or int conversion failed.');
+            throw ArgumentError('Time contains non-numeric values or int conversion failed.');
         
         if(hours < 0 || hours > 23)
-            throw ArgumentError('yotei error: Hours is invalid.');
+            throw ArgumentError('Hours is invalid.');
         if(minutes < 0 || minutes > 59)
-            throw ArgumentError('yotei error: Minutes is invalid.');
+            throw ArgumentError('Minutes is invalid.');
         
         return [hours, minutes];
     }
