24bba8bf8415df23517305b84fd7aca502feaaaf
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-23 18:05:53 -0300

    fix: fixed async missing

--- bin/yotei.dart
+++ bin/yotei.dart
@@ -1,6 +1,6 @@
 import 'package:yotei/cli/cli_interface.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   final cli = CliInterface(arguments);
   cli.run();
 }
