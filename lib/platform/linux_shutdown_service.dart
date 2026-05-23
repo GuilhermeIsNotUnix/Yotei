5386bfda474c3cd94d73c52c0969ef778d77839f
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-22 21:52:04 -0300

    feat: add LinuxShutdownService class to manage Linux specific commands

---
+++ lib/platform/linux_shutdown_service.dart
@@ -0,0 +1,38 @@
+class LinuxShutdownService {
+    Future<void> scheduleShutdown(DateTime targetTime) async {
+        final process = await Process.run("shutdown", ["-h", "${targetTime.hour}:${targetTime.minute}"]);
+
+        print("Exit code: ${process.exitCode}");
+        print("Output:\n${process.stdout}");
+
+        if(process.stderr.isNotEmpty) {
+            print("Errors:${process.stderr}\n");
+        }
+    }
+
+    Future<void> shutdownNow(bool debugMode) async {
+        if(!debugMode) {
+            final process = await Process.run("shutdown", ["-h", "now"]);
+
+            print("Exit code: ${process.exitCode}");
+            print("Output:\n${process.stdout}");
+
+            if(process.stderr.isNotEmpty) {
+                print("Errors:${process.stderr}\n");
+            }
+        } else {
+            print("yotei debug mode:\n\n Command set to be executed: shutdown -h now");
+        }
+    }
+
+    Future<void> testEchoCommand(String message) async {
+        final process = await Process.run("echo", [message]);
+
+        print("Exit code: ${process.exitCode}");
+        print("Output:\n${process.stdout}");
+
+        if(process.stderr.isNotEmpty) {
+            print("Errors:${process.stderr}\n");
+        }
+    }
+}
