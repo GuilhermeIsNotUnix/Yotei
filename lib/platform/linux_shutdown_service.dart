8d10ca197f40355d4e5cd4547c7a38aec1f1670d
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-23 20:42:45 -0300

    refactor/fix: all methods were refactored for a better debug logging, prints were deleted (its not this class responsability to print information, but the interface...

--- lib/platform/linux_shutdown_service.dart
+++ lib/platform/linux_shutdown_service.dart
@@ -1,60 +1,62 @@
 import 'dart:io';
 
 class LinuxShutdownService {
-    Future<void> scheduleShutdown(DateTime targetTime) async {
+    Future<void> scheduleShutdown(DateTime targetTime, bool debugMode) async {
         final hours = targetTime.hour.toString().padLeft(2, '0');
         final minutes = targetTime.minute.toString().padLeft(2, '0');
         
         final process = await Process.run("shutdown", ["-h", "$hours:$minutes"]);
 
-        print("Exit code: ${process.exitCode}");
-        print("Output:\n${process.stdout}");
+        if(process.exitCode != 0) {
+            throw Exception("yotei error: ${process.stderr}");
+        }
 
-        if(process.stderr.isNotEmpty) {
-            print("Errors:${process.stderr}\n");
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
         }
     }
 
     Future<void> shutdownNow(bool debugMode) async {
-        if(debugMode) {
-            print("yotei debug mode:\n\n Command set to be executed: shutdown -h now");
-            return;
-        }
-
         final process = await Process.run("shutdown", ["-h", "now"]);
 
-        print("Exit code: ${process.exitCode}");
-        print("Output:\n${process.stdout}");
+        if(process.exitCode != 0) {
+            throw Exception("yotei error: ${process.stderr}");
+        }
 
-        if(process.stderr.isNotEmpty) {
-            print("Errors:${process.stderr}\n");
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
         }
     }
 
     Future<void> cancelShutdown(bool debugMode) async {
-        if(debugMode) {
-            print("yotei debug mode:\n\n Command set to be executed: shutdown -c");
-            return;
-        }
-
         final process = await Process.run("shutdown", ["-c"]);
 
-        print("Exit code: ${process.exitCode}");
-        print("Output:\n${process.stdout}");
+        if(process.exitCode != 0) {
+            throw Exception("yotei error: ${process.stderr}");
+        }
 
-        if(process.stderr.isNotEmpty) {
-            print("Errors:${process.stderr}\n");
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
         }
     }
 
-    Future<void> testEchoCommand(String message) async {
+    Future<void> testEchoCommand(String message, bool debugMode) async {
         final process = await Process.run("echo", [message]);
 
-        print("Exit code: ${process.exitCode}");
-        print("Output:\n${process.stdout}");
+        if(process.exitCode != 0) {
+            throw Exception("yotei error: ${process.stderr}");
+        }
 
-        if(process.stderr.isNotEmpty) {
-            print("Errors:${process.stderr}\n");
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
         }
     }
 }
