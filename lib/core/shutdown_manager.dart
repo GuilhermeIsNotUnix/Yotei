c00bb76c487a5cc1ff822e8484281c27c2f53340
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-23 17:35:48 -0300

    feat: add ShutdownManager() class to abstract the management of shutdown services outside of CLI or GUI interfaces...

---
+++ lib/core/shutdown_manager.dart
@@ -0,0 +1,85 @@
+import 'package:yotei/core/time_parser.dart';
+import 'package:yotei/core/time_scheduler.dart';
+import 'package:yotei/platform/platform_detector.dart';
+import 'package:yotei/platform/linux_shutdown_service.dart';
+
+class ShutdownManager {
+    Future<void> scheduleShutdown(String givenTime) {
+        final os = getOperatingSystem();
+        final parsedTime = TimeParser.parseTime(givenTime);
+        final targetTime = TimeScheduler.constructDateTime(parsedTime);
+
+        switch(os) {
+            case OperatingSystem.linux:
+                final service = LinuxShutdownService();
+                await service.scheduleShutdown(targetTime);
+
+                break;
+            case OperatingSystem.windows:
+                //...
+                
+                break;
+            case OperatingSystem.macOS:
+                //...
+                
+                break;
+        }
+    }
+
+    Future<void> shutdownNow() {
+        final os = getOperatingSystem();
+
+        switch(os) {
+            case OperatingSystem.linux:
+                final service = LinuxShutdownService();
+                await service.shutdownNow();
+
+                break;
+            case OperatingSystem.windows:
+                //...
+                
+                break;
+            case OperatingSystem.macOS:
+                //...
+                
+                break;
+        }
+    }
+
+    Future<void> cancelShutdown() {
+        final os = getOperatingSystem();
+
+        switch(os) {
+            case OperatingSystem.linux:
+                final service = LinuxShutdownService();
+                await service.cancelShutdown();
+
+                break;
+            case OperatingSystem.windows:
+                //...
+                
+                break;
+            case OperatingSystem.macOS:
+                //...
+                
+                break;
+        }
+    }
+
+
+    OperatingSystem getOperatingSystem() {
+        final detector = PlatformDetector();
+        final os = detector.detectOperatingSystem();
+
+        return os;
+    }
+
+    /* void handleShutdownCommand() {
+        if(arguments.length > 1) {
+            final parsedTime = TimeParser.parseTime(arguments[1]);
+            print('Scheduling shutdown at $parsedTime');
+        } else {
+            throw ArgumentError('yotei error: Missing time argument.');
+        }
+    } */
+}
