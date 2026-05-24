bac899807c05053478bf58344e0aa205334a4206
GuilhermeIsNotUnix <guilherme.faura@gmail.com>
2026-05-24 00:15:26 -0300

    feat: add macOSShutdownService class as a shutdown service layer for MacOS.

---
+++ lib/platform/macos_shutdown_service.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+class MacOSShutdownService {
+    Future<void> scheduleShutdown(DateTime targetTime, bool debugMode) async {
+        final hours = targetTime.hour.toString().padLeft(2, '0');
+        final minutes = targetTime.minute.toString().padLeft(2, '0');
+        
+        final process = await Process.run("shutdown", ["-h", "$hours:$minutes"]);
+
+        if(process.exitCode != 0) {
+            throw Exception("${process.stderr}");
+        }
+
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
+        }
+    }
+
+    Future<void> shutdownNow(bool debugMode) async {
+        final process = await Process.run("shutdown", ["-h", "now"]);
+
+        if(process.exitCode != 0) {
+            throw Exception("${process.stderr}");
+        }
+
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
+        }
+    }
+
+    Future<void> cancelShutdown(bool debugMode) async {
+        final process = await Process.run("shutdown", ["-c"]);
+
+        if(process.exitCode != 0) {
+            throw Exception("${process.stderr}");
+        }
+
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
+        }
+    }
+
+    Future<void> testEchoCommand(String message, bool debugMode) async {
+        final process = await Process.run("echo", [message]);
+
+        if(process.exitCode != 0) {
+            throw Exception("${process.stderr}");
+        }
+
+        if(debugMode) {
+            print("Exit Code: ${process.exitCode}");
+            print("Output: ${process.stdout}");
+            print("Errors/Warnings: ${process.stderr}");
+        }
+    }
+}
